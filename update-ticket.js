2722ddea31e9900b18a4e973c00eb15791a86ce7
Olga <olyakrylo24@gmail.com>
2022-08-17 14:07:34 +0300

    fix getting commits

--- update-ticket.js
+++ update-ticket.js
@@ -1,5 +1,6 @@
 const fetch = require("node-fetch");
 const github = require("@actions/github");
+const exec = require("@actions/exec");
 
 require("dotenv").config();
 
@@ -20,24 +21,21 @@ const headers = {
 // }).then((response) => response.json()).then((res) => console.log(res));
 
 const updateTicket = async () => {
-  const commits = github.context.payload.commits?.map((c) => {
-    return `${c.id} ${c.author.name} ${c.message}`
-  }) ?? [];
+  const currentTag = github.context.payload.ref?.replace("refs/tags/", "") ?? "";
+  console.info("Tag", currentTag)
 
+  const commits = await getCommits(currentTag);
   console.info("Commits", commits);
 
-  let currentTag = github.context.payload.ref?.replace("refs/tags/", "") ?? "";
-  currentTag = currentTag.replace("rc-", "");
-
-  console.info("Tag", currentTag)
+  console.log(github.context.payload);
 
   const pusherName = github.context.payload.pusher?.name;
   const pushDate = new Date().toLocaleDateString();
+  console.info("Pusher");
+  console.info("Date", pushDate);
 
-  console.info("Info", pusherName, pushDate);
-
-  const summary = `Релиз №${currentTag} от ${pushDate}`;
-  const description = `Ответственный за релиз: ${pusherName}\n---\nКоммиты, попавшие в релиз:\n${commits.join("\n")}`;
+  const summary = `Релиз №${currentTag.replace("rc-", "")} от ${pushDate}`;
+  const description = `Ответственный за релиз: ${pusherName}\n---\nКоммиты, попавшие в релиз:\n${commits}`;
 
   fetch(`${HOST}/v2/issues/${TICKET_ID}`, {
     method: "PATCH",
@@ -49,6 +47,36 @@ const updateTicket = async () => {
   }).then((response) => response.json()).then((res) => {
     console.info("Ticket is successfully updated");
   });
+}
+
+const getCommits = async (currentTag) => {
+  const tags = (await execCommand('git', ['tag'])).split("\n").filter(Boolean);
+  const index = tags.indexOf(currentTag);
+  const commitsFilter = tags.length === 1 ? currentTag : `${currentTag}...${tags[index - 1]}`;
+  const releaseCommits = await execCommand('git', ['log', '--pretty=format:"%h %an %s"', commitsFilter]);
+  return releaseCommits.replace(/"/g, "");
+}
+
+const execCommand = async (command, options) => {
+  let resString = "";
+  let errString = "";
+
+  await exec.exec(command, options, {
+    listeners: {
+      stdout: (data) => {
+        resString += data.toString();
+      },
+      stderr: (data) => {
+        errString += data.toString();
+      }
+    }
+  });
+
+  if (errString) {
+    throw new Error(`Unable to execute ${command} ${options}`)
+  }
+
+  return resString;
 }
 
 updateTicket().then(() => console.info("Successfully done!"));
