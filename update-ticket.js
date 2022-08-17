1cc442ceb022014d8f509c5f05e47ac3d2f2d4e7
Olga <olyakrylo24@gmail.com>
2022-08-17 13:47:21 +0300

    update logging

--- update-ticket.js
+++ update-ticket.js
@@ -24,11 +24,17 @@ const updateTicket = async () => {
     return `${c.id} ${c.author.name} ${c.message}`
   }) ?? [];
 
+  console.info("Commits", commits);
+
   let currentTag = github.context.payload.ref?.replace("refs/tags/", "") ?? "";
   currentTag = currentTag.replace("rc-", "");
 
+  console.info("Tag", currentTag)
+
   const pusherName = github.context.payload.pusher?.name;
   const pushDate = new Date().toLocaleDateString();
+
+  console.info("Info", pusherName, pushDate);
 
   const summary = `Релиз №${currentTag} от ${pushDate}`;
   const description = `Ответственный за релиз: ${pusherName}\n---\nКоммиты, попавшие в релиз:\n${commits.join("\n")}`;
